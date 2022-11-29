ec770495cdc3bb139570523b90b524e235b0df63
Tim <tim@tim-u.me>
2022-11-29 01:04:13 +0100

    added Metar Downloader for Tests later

--- MetarSharp.Tests/GetMetars.cs
+++ MetarSharp.Tests/GetMetars.cs
@@ -3,33 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace MetarSharp.Tests
 {
-    internal class GetMetars
+    public class Airport
     {
-        public static List<string> Metars()
+        public string Icao { get; set; }
+        public string Iata { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Elevation { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Tz { get; set; }
+    }
+    public class Rootobject
+    {
+        internal class GetMetars
         {
-            var letters = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-
-            List<string> returnMetars = new List<string>();
-
-            Random rand = new Random();
-
-
-            List<string> airports = new List<string>();
-            for (int x = 0; x < 500; x++)
+            public static async Task<List<string>> Metars()
             {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (int y = 0; y < 4; y++)
+                var letters = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+                List<string> returnMetars = new List<string>();
+
+                Random rand = new Random();
+
+
+                List<string> airports = new List<string>();
+                for (int x = 0; x < 500; x++)
                 {
-                    int letterIndex = rand.Next(0, 26);
-                    stringBuilder.Append(letters[letterIndex]);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int y = 0; y < 4; y++)
+                    {
+                        int letterIndex = rand.Next(0, 26);
+                        stringBuilder.Append(letters[letterIndex]);
+                    }
+
+                    airports.Add(stringBuilder.ToString());
                 }
 
-                airports.Add(stringBuilder.ToString());
+                string webData = null;
+                WebClient wc = new WebClient();
+                foreach (var airport in airports)
+                {
+                    byte[] raw = wc.DownloadData("https://metar.vatsim.net/" + airport);
+                    webData = Encoding.UTF8.GetString(raw);
+
+                    returnMetars.Add(webData);
+
+                    await Task.Delay(5000);
+                }
+                return returnMetars;
             }
-            return returnMetars;
-        }
+
+            private static List<string> getJson()
+            {
+                List<string> list = new List<string>();
+
+                string webData = null;
+                WebClient wc = new WebClient();
+                byte[] raw = wc.DownloadData("https://pkgstore.datahub.io/core/airport-codes/airport-codes_json/data/9ca22195b4c64a562a0a8be8d133e700/airport-codes_json.json");
+                webData = Encoding.UTF8.GetString(raw);
+
+                var icaoList = JsonConvert.DeserializeObject<List<Airport>>(webData);
+
+                return list;
+            }
+        }     
     }
 }
--- MetarSharpDebugger/Program.cs
+++ MetarSharpDebugger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -17,15 +18,15 @@ internal class Program
 
         public static string RestOfMetar { get; set; }
     }
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         ///Just for diagnostics
         Stopwatch timer = new Stopwatch();
         timer.Start();
-        
+
         //You can enter your metars here
         Metar metar = ParseMetar.ParseFromString("KCOU 182054Z 18011KT 10SM 0050E VCTS FEW031 FEW055 BKN120 25/22 A2995 WS R25 RESN BLU RMK HALLO");
-        
+
         ///Just for diagnostics/to check execution time 
         timer.Stop();
         var executeTime = timer.ElapsedMilliseconds;
