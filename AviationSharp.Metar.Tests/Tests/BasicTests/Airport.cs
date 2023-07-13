250a4ada3b5336e1892bb503b78ae804d1b3b1cc
Tim <tim@tim-u.me>
2023-07-13 18:47:59 +0200

    renamed everything (hopefully)

---
+++ AviationSharp.Metar.Tests/Tests/BasicTests/Airport.cs
@@ -0,0 +1,18 @@
+﻿namespace AviationSharp.Metar.Tests.Tests.Airport
+{
+    internal class Airport
+    {
+
+        [Test]
+        public void CheckAirportIsNotNull_ReturnsTrue()
+        {
+            Assert.That(MetarsParsed.All(x => x.Airport != null));
+        }
+
+        [Test]
+        public void CheckAirportLength_ReturnsFour()
+        {
+            Assert.That(MetarsParsed.All(x => x.Airport.Length == 4), "Airport length was: ");
+        }
+    }
+}
---
+++ AviationSharp.Metar/ParseMetar/Additional/RecentWeather.cs
@@ -0,0 +1,20 @@
+namespace MetarSharp.Parse.Additional
+{
+    internal class RecentWeatherParse
+    {
+        internal static RecentWeather Parse(GroupCollection groups)
+        {
+            var recent = new RecentWeather
+            {
+                RecentWeatherRaw = groups[2].Value,
+
+                RecentWeatherTypeRaw = groups[4].Value
+            };
+
+            var weatherDecoded = GetWeatherType.Get(groups[4].Value).Item2;
+            recent.RecentWeatherDecoded = $"Recent {weatherDecoded}";
+
+            return recent;
+        }
+    }
+}
